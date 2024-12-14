98990d3f5eac630dbfbf356b80c4653d096e8f47
Kamil Hebda <kamilhebda28@gmail.com>
2024-12-14 21:10:04 +0100

    Refactor API gateway to support dynamic microservice routing and remove unused config file

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -3,33 +3,29 @@ import requests
 
 app = Flask(__name__)
 
-# Adresy mikroserwisów (dla przykładu)
-#MICROSERVICE_CHAT_BOT_URL = "http://localhost:5001"
-MICROSERVICE_EDUCATION_URL = "http://127.0.0.1:5001"
-
-@app.route('/lesson/<int:lesson_id>', methods=['GET'])
-def lesson(lesson_id):
-    """Endpoint rejestracji użytkownika"""
-
-    # Przekazanie zapytania do mikroserwisu A (obsługuje użytkowników)
-    response = requests.post(f"{MICROSERVICE_EDUCATION_URL}/lesson/{lesson_id}")
+MICROSERVICES = {
+    "education": "http://localhost:5001",
+    "chatbot": "http://localhost:5002",
+    "helpdesk": "http://localhost:5003"
+}
     
-    if response.status_code == 200:
-        return jsonify(response.json()), 200
-    else:
-        return jsonify({"error": "Sperma"}), response.status_code
-    
+@app.route('/api/<service>/<path:endpoint>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(service, endpoint):
+    if service not in MICROSERVICES:
+        return jsonify({"error": "Service not found"}), 404
 
-
-@app.route('/lessons')
-def index():
-    """Strona główna - lista lekcji"""
-    response = requests.post(f"{MICROSERVICE_EDUCATION_URL}/lessons")
-
-    if response.status_code == 200:
-        return jsonify(response.json()), 200
-    else:
-        return jsonify({"error": "Sperma"}), response.status_code
+    # Budowanie żądania do mikroserwisu
+    url = f"{MICROSERVICES[service]}/{endpoint}"
+    response = requests.request(
+        method=request.method,
+        url=url,
+        headers={key: value for key, value in request.headers if key != 'Host'},
+        json=request.get_json(),
+        params=request.args
+    )
+    print("Response status:", response.status_code)
+    print("Response content:", response.text)
+    return jsonify(response.json()), response.status_code
     
     
 if __name__ == '__main__':
