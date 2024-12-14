c73d17b99776b1a628a2f82a81b701e68b6ad320
pawel-rus <pawelrus.637@gmail.com>
2024-12-14 14:05:44 +0100

    env variables

--- backend/config.py
+++ backend/config.py
@@ -1,13 +1,14 @@
 import os
 from urllib.parse import quote_plus
+from dotenv import load_dotenv
 
 #postgresql://postgres:GWnQGFuMpUnZxayhsYrtEdGoOnXUxCpd@junction.proxy.rlwy.net:37394/railway
-
-password = quote_plus(os.getenv('DB_PASSWORD', 'API_KEY'))
-db_host = os.getenv('DB_HOST', 'junction.proxy.rlwy.net')
-db_port = os.getenv('DB_PORT', '37394')
-db_name = os.getenv('DB_NAME', 'railway')
-db_user = os.getenv('DB_USER', 'postgres')
+load_dotenv()
+password = quote_plus(os.getenv('DB_PASSWORD'))
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
 
 DATABASE_URL = f"postgresql://{db_user}:{password}@{db_host}:{db_port}/{db_name}"
 
