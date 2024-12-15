d000ce6c903f69d3e7e563d315b04c39334dd53c
pawel-rus <pawelrus.637@gmail.com>
2024-12-15 02:31:59 +0100

    update

--- education_service/models.py
+++ education_service/models.py
@@ -23,3 +23,61 @@ class Lesson(db.Model):
     def get_lesson_by_id(cls, lesson_id):
         """Metoda do pobierania lekcji na podstawie ID"""
         return cls.query.get(lesson_id)  
+
+
+
+
+class Tutorial(db.Model):
+    __bind_key__ = 'db'
+    __tablename__ = 'tutorials'
+
+    id = db.Column(db.Integer, primary_key=True)
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.id'), nullable=False, unique=True)  # Unikalny klucz obcy
+    steps = db.Column(db.JSON, nullable=False)  # JSON przechowujący kroki tutorialu
+
+    def __repr__(self):
+        return f'<Tutorial for Lesson {self.lesson_id}>'
+
+    @classmethod
+    def get_tutorial_by_lesson_id(cls, lesson_id):
+        """Metoda do pobierania tutorialu na podstawie ID lekcji"""
+        return cls.query.filter_by(lesson_id=lesson_id).first()
+
+    @classmethod
+    def get_tutorial_by_id(cls, tutorial_id):
+        """Metoda do pobierania tutorialu na podstawie jego ID"""
+        return cls.query.get(tutorial_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Quiz(db.Model):
+#     __bind_key__ = 'db'
+#     __tablename__ = 'quizzes'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     question = db.Column(db.String(255), nullable=False)
+#     options = db.Column(db.JSON, nullable=False)  # Przechowujemy opcje jako JSON
+#     correct_answer = db.Column(db.String(255), nullable=False)  # Poprawna odpowiedź
+#     lesson_id = db.Column(db.Integer, db.ForeignKey('lessons.id'), nullable=False)
+
+#     def __repr__(self):
+#         return f'<Quiz {self.question}>'
+
+#     @classmethod
+#     def get_quizzes_by_lesson_id(cls, lesson_id):
+#         """Metoda do pobierania wszystkich quizów dla lekcji na podstawie ID"""
+#         return cls.query.filter_by(lesson_id=lesson_id).all()
