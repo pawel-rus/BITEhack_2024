40efbc2f658f51f8eeb560981a0d1568e7a0d8c1
pawel-rus <pawelrus.637@gmail.com>
2024-12-15 05:08:26 +0100

    fckreact

--- front2/my-vite-react-app/src/App.jsx
+++ front2/my-vite-react-app/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ChatBubble from './components/ChatBubble';
+import Courses from './components/Courses';
+
 import './styles.css';
 
 function Home() {
     return <div>Home page</div>
 }
-function Courses() {
-    return <div>Courses page</div>
-}
+
 function Assistant() {
     return <div>Assistant page</div>
 }
